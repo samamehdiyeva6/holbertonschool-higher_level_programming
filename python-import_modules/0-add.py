afaaaf3a840e0318d912f826b4a4faf9bd4c5b5f
Your Name <you@example.com>
2025-12-07 16:38:14 +0000

    done

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
+from add_0 import add
 
-a = 1
-b = 2
-print(f"{a} + {b} = ", add(a, b))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
