2ef7e8eca9bb6d9932f615eedd4b05603bf313f4
Your Name <you@example.com>
2025-12-08 10:38:39 +0000

    done

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from argv_2 import arg
+
+if __name__ = "__main__":
+    argv = "Hello"
+    length  = len(argv)
+    if length % 2 == 0:
+        print(f"{length} arguments:")
+    elif length % 2 != 0:
+        print(f"{length} argument:")
+    else:
+        print("0 arguments.")
+
+    list = argv.split()
+    for i in range(1, length+1):
+        print("{}: {}".format(i, list[i-1]))
