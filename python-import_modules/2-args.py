ec5ed5696d2757133593493e6c4a155a906abd3d
Your Name <you@example.com>
2025-12-08 10:46:47 +0000

    done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     argv = "Hello"
-    length  = len(argv)
-    if length % 2 == 0:
+    list = argv.split()
+    if len(list) % 2 == 0:
         print(f"{length} arguments:")
-    elif length % 2 != 0:
+    elif len(list) % 2 != 0:
         print(f"{length} argument:")
     else:
         print("0 arguments.")
 
-    list = argv.split(" ")
-    for i in range(0, length):
+    for i in range(0, len(list)):
         print("{}: {}".format(i+1, list[i]))
