e8b651ed628723b1e4c85a8402ab9dca4273636f
Your Name <you@example.com>
2025-12-08 14:21:39 +0000

    done

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list)-1:
-        return None
-    else:
-        return my_list[idx]
+if __name__ == "__main__":
+    def element_at(my_list, idx):
+        if idx < 0 or idx > len(my_list)-1:
+            return None
+        else:
+            return my_list[idx]
 
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
