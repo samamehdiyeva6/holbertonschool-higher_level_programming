c96ad889db7fb6e0adeb28bbe8b7c5f4d66c4a59
Your Name <you@example.com>
2025-12-08 11:02:10 +0000

    done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
 
     argv = sys.argv[1:]
-    if len(argv) % 2 == 0 and len(argv) != 0:
+    if len(argv) != 0 and len(argv) != 1:
         print(f"{len(argv)} arguments:")
-    elif len(argv) % 2 != 0:
+    elif len(argv) == 1:
         print(f"{len(argv)} argument:")
     else:
         print("0 arguments.")
