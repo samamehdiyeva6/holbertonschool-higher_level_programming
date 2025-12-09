233fc5e0f2dd0999d599ef8db510d3c75fad4696
Sama Mehdiyeva <105722149+samamehdiyeva6@users.noreply.github.com>
2025-12-09 18:52:39 +0400

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    # Əgər index səhvdirsə, heç bir şey etməyəcək
     if idx < 0 or idx >= len(my_list):
         return my_list
-
-    # Verilən index-dəki elementi silirik
-    my_list = my_list[:idx] + my_list[idx+1:]
+    del my_list[idx]
     return my_list
