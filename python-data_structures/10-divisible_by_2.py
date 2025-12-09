8eb791ab4039588c4e6d177917de38822beb9471
Your Name <you@example.com>
2025-12-09 13:50:39 +0000

    done

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
+    new_list = 
     for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
-            new_list[i] = True
+            new_list.insert(i, True)
         else:
-            new_list[i] = False
+            new_list.insert(i, False)
     return new_list
