2ac2353623c7ebe955169b3eab7a0ae6d506931f
Your Name <you@example.com>
2025-12-09 11:28:41 +0000

    done

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list = my_list
-    if idx > 0 or idx > len(my_list)-1:
-        return list
+    if idx > 0 or idx >= len(my_list):
+        return my_list
     else:
         my_list[idx] = element
-        return list
+        return my_list
