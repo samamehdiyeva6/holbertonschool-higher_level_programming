21b6ddcc3e8c44d17f155149cfb4a38a3597856e
Your Name <you@example.com>
2025-12-09 14:43:17 +0000

    done

--- python-data_structures/11-main.py
+++ python-data_structures/11-main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
+delete_at = __import__('11-delete_at').delete_at  # Funksiyanı idxal edirik
 
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_list = delete_at(my_list, idx)
-    print(new_list)
+if __name__ == "__main__":  # Bura düzəldilmiş yazılışdır
+    my_list = [1, 2, 3, 4, 5]  # Siyahı
+    idx = 3  # Silinəcək elementin indexi
+    new_list = delete_at(my_list, idx)  # delete_at funksiyasını çağırırıq
+
+    # Nəticəni bir dəfə çap edirik
+    print(new_list, end="")  # Yeni sətir əlavə etməmək üçün end="" istifadə edirik
+    print(my_list, end="")   # Eyni şəkildə ikinci siyahını çap edirik
