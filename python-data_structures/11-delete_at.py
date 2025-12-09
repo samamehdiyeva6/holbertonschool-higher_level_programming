81c9ca9f33d4f997a098d62d02c409e5ee6703ca
Your Name <you@example.com>
2025-12-09 14:45:49 +0000

    done

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    # Əgər index səhvdirsə, heç bir şey etməyəcək
     if idx < 0 or idx >= len(my_list):
         return my_list
 
+    # Verilən index-dəki elementi silirik
     my_list = my_list[:idx] + my_list[idx+1:]
     return my_list
