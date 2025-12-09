02702324e57cd0d593ed969b99f5ce86fe533fb0
Your Name <you@example.com>
2025-12-09 14:12:43 +0000

    done

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,4 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 and idx >= len(my_list):
         return my_list
     else:
-        return my_list[:idx]+ my_list{idx+1:}
+        return my_list[:idx]+ my_list[idx+1:]
