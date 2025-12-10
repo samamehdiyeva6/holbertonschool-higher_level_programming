2710e64046caf6af3262262bef8930bf73f011ef
Your Name <you@example.com>
2025-12-10 13:52:51 +0000

    done

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
-    for i in range(0, len(new_matrix)):
-        for j in range(0, len(new_matrix[i])):
-            new_matrix[i][j] = new_matrix[i][j] ** 2
-    return new_matrix
+    return ((x ** x for x in row) for row in matrix)
