454f05f224b1fd5f0eb597dc42c74171257d34e6
Your Name <you@example.com>
2025-12-13 18:31:55 +0000

    done

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
         list.append(i)
     list.sort()
     for j in list:
-        print(j + ": " + a_dictionary.keys(j))
+        print(j + ": " + a_dictionary.get(j))
