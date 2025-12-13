a1bbc8f64ef1a1dac8f4eb83852b8945c86074b3
Your Name <you@example.com>
2025-12-13 19:46:20 +0000

    done

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary.get(key) == None:
-        a_dictionary.update({key: value})
-    else:
-        a_dictionary.get(key) == value
-    return a_dictionary
+    return (a_dictionary.update({key : value}))
