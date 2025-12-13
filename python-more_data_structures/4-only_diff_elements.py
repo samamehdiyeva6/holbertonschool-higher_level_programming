c44d7710fe447a69f4836d86dcf7f39793bd68da
Your Name <you@example.com>
2025-12-13 15:40:52 +0000

    donr

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    list = []
-    for i in set_1:
-        for j in set_2:
-            if i != j:
-                list.append(i)
-    return list
+    set = set_1.symmetric_difference(set_2)
+    return set
