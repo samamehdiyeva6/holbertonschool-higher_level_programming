7794c314f8ff64be150dc9773e78491c68c7718a
Your Name <you@example.com>
2025-12-14 07:59:04 +0000

    done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary.values()) == 0:
+    if len(list(a_dictionary.values())) == 0:
         return None
     else:
         max = list(a_dictionary.values())[0]
