89be6e2b45e987ae5935562cff8cf6de09daf127
Your Name <you@example.com>
2025-12-14 08:24:16 +0000

    done

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -3,4 +3,4 @@ def multiply_list_map(my_list=[], number=0):
     new_list = []
     for i in my_list:
         new_list.append(i * number)
-    return new_list
+    return map(multiply_list_map, my_list, number)
