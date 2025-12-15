68af2033be19402e406a44e888b3a83dd0cf2100
Your Name <you@example.com>
2025-12-15 14:53:58 +0000

    done

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    else:
-        print("{}".format(i), sep=", ")
+print(", ".join("{:02}".format(i) for i in range(100)))
