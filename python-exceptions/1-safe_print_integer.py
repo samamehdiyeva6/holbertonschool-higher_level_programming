ad10adbc31811637b1a880b46c8704960f310013
Your Name <you@example.com>
2025-12-15 11:13:39 +0000

    done

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,7 @@ def safe_print_integer(value):
     try:
         if value.isdigit():
             return True
+            print("{:d}".format(value))
         else:
             return False
     except Exception:
