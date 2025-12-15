6e983baaa27bb8b375eff95f04fc8b9731d13ade
Your Name <you@example.com>
2025-12-15 12:04:58 +0000

    done

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(0, x):
             print("{:d}".format(my_list[i]), end="")
             count += 1
-    except Exception:
-        pass
+    except Exception as ex:
+        print(ex.args)
     print()
     return count
