47b8cd458eb5d1e426901c3063dd66eb2f8fb5c8
Your Name <you@example.com>
2025-12-15 10:18:57 +0000

    done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        string = ""
         for i in range(0, x):
-            print(int(my_list[i]), end = "")
+            string += my_list[i]
+        print(int(string))
     except IndexError:
         print("This index doesn't exist")
