c5a2fd7fc5eab11a349a41042c568c51270b2174
Your Name <you@example.com>
2025-12-15 11:16:28 +0000

    done

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value.isdigit():
-            return True
-            print("{:d}".format(value))
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
+    else:
+        return False
+
     except Exception:
         pass
