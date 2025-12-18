556e5a8965d5d5db1cd2d767839c7ea09b2c7272
Your Name <you@example.com>
2025-12-18 15:01:13 +0000

    gitpusher

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -30,7 +30,7 @@ class Square:
         if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if self.__size < 0:
             raise ValueError("size must be >= 0")
 
         else:
