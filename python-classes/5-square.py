76ee1a65bfae57c6522714bd74ca8798d8b9a831
Your Name <you@example.com>
2025-12-18 15:15:07 +0000

    gitpusher

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This is an empty Square class."""
+
+
+class Square:
+    """Defines a square with a size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes a square with a given size."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initializes a square with a given size."""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("\n")
+        else:
+            string = ""
+            for i in range(self.__size):
+                string += "#"
+            for j in range(self.__size):
+                print(string + "\n")
