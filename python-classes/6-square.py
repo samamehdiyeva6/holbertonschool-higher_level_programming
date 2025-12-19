d0e91b91f3b918944649a62052569f4154b5bc83
Your Name <you@example.com>
2025-12-19 20:37:02 +0000

    gitpusher

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,4 +51,4 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = position
+            self.__position = value
