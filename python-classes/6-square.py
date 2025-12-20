30a91169d58d186ae4b6249bfe3b670c1a231ff5
Your Name <you@example.com>
2025-12-20 11:32:17 +0000

    gitpusher

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,7 +51,7 @@ class Square:
         if self.__size == 0:
             print("")
         
-        print("") for i in range(0, self.__position[1])
+        [print("") for i in range(0, self.__position[1])]
         for j in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
