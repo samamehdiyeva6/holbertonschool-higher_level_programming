27027bcf94a79058ae2e80bf00125c52bf9f2d58
Your Name <you@example.com>
2025-12-20 12:36:01 +0000

    gitpusher

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,14 +36,14 @@ class Square:
     @position.setter
     def position(self, value):
         "ghfhfh"
-        if 
-        (not isinstance(value, tuple)
-            or len(value) != 2 or
-            not all(isinstance(i, int) and i >= 0 for i in value)
-        ):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if (
+    not isinstance(value, tuple)
+    or len(value) != 2
+    or not all(isinstance(i, int) and i >= 0 for i in value)
+):
+    raise TypeError("position must be a tuple of 2 positive integers")
+else:
+    self.__position = value
 
     def area(self):
         "jhfhgf"
