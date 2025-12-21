2cb6b8134119a1ac7684bab90fe352bdbc87e3c7
Your Name <you@example.com>
2025-12-21 09:45:36 +0000

    gitpusher

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""hsagdjhgsdja"""
+
+
+class Rectangle:
+    "sdfsdfsd"
+    pass
+
+    def __init__(self, width=0, height=0):
+        "jhkdhf"
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        self.__width = width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        self.__height = height
+
+    def height(self, value):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
