fc8fb6fd072d911f597998e5d621b913c6cd7eba
Your Name <you@example.com>
2025-12-22 14:58:14 +0000

    gitpusher

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
         return Rectangle(self.__width, self.__height)
 
     def __repr__(self):
-        return Rectangle.eval(self)
+        print("Rectangle({}, {})".format(self.__width, self.__height))
