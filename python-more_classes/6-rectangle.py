36d8ed814029762c3670c42028688e6f073d3ec5
Your Name <you@example.com>
2025-12-23 12:40:30 +0000

    gitpusher

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         "jhkdhf"
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
