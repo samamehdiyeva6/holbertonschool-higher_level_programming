135cfd889b679ae91447a84f9f79ac827dc66790
Your Name <you@example.com>
2025-12-23 12:22:16 +0000

    gitpusher

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""hsagdjhgsdja"""
+
+
+class Rectangle:
+    "sdfsdfsd"
+    pass
+
+    def __init__(self, width=0, height=0):
+        "jhkdhf"
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        "jhkg"
+        return self.__width * self.__height
+
+    def perimeter(self):
+        "hgjgjg"
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        lines = []
+        for h in range(self.__height):
+            lines.append("#" * self.__width)
+
+        return "\n".join(lines)
+
+    def print(self):
+        if self.__height == 0 or self.__width == 0:
+            print("")
+            return
+        for h in range(0, self.__height):
+            for w in range(0, self.__width):
+                print("#", end="")
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
