947cefb6bd772c093363e662e51af3ae0bee3a1e
Your Name <you@example.com>
2025-12-23 12:39:32 +0000

    gitpusher

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -68,5 +68,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
