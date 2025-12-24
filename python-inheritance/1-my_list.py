31bf439b12973362920881b9143c16a4c1ac4628
Your Name <you@example.com>
2025-12-24 14:06:40 +0000

    gitpusher

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"jfdkjdf"
+"""MyList module"""
 
 
 class MyList(list):
-    "jhkkkkh"
+    """MyList class that inherits from list"""
+
     def print_sorted(self):
+        """Prints the list in ascending sorted order"""
         print(sorted(self))
