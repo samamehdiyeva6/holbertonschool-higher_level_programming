f1fec741c6e3d9e47d5fca3e9f536dc7fb23e6f6
Your Name <you@example.com>
2025-12-26 14:02:02 +0000

    gitpusher

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,5 +10,5 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         if not type(value):
             raise TypeError("<name> must be an integer")
-        if value <= 0:
+        if int(value) <= 0:
             raise ValueError("<name> must be greater than 0")
