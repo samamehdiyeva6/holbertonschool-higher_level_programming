ee0504ace2ae35f9971d4bbbe05e62db34f0d70b
Your Name <you@example.com>
2025-12-26 14:20:53 +0000

    gitpusher

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,19 +2,10 @@
 "hjdsfkhd"
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     "dgdfgd"
     def __init__(self, width, height):
         if (type(width) and type(height) is int) and width * height > 0:
             self.__width = width
             self.__height = height
         raise Exception
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif int(value) <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
