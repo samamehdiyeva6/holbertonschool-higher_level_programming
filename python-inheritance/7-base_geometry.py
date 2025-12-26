a9e21d90a2fdec13ee814fec798647fd48edbeff
Your Name <you@example.com>
2025-12-26 14:01:03 +0000

    gitpusher

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not type(self.value):
+        if not type(value):
             raise TypeError("<name> must be an integer")
-        if self.value <= 0:
+        if value <= 0:
             raise ValueError("<name> must be greater than 0")
