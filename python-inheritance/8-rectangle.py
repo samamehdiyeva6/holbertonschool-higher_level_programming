ebcd4dd766a4417431a0b42a2e35dce7ed19e839
Your Name <you@example.com>
2025-12-26 14:34:18 +0000

    gitpusher

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,11 +4,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     "dgdfgd"
+
     def __init__(self, width, height):
-        if ((type(width) and type(height)) is int):
-            if width < 0:
-                raise ValueError("width must be greater than 0")
-            if height < 0:
-                raise ValueError("height must be greater than 0")
-            self.__width = width
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
