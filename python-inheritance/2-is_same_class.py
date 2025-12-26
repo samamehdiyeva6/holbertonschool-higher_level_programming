ab9009ad8095d0d66f7a0577755aabc3d3dba94f
Your Name <you@example.com>
2025-12-26 13:09:13 +0000

    gitpusher

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,5 +6,7 @@ def is_same_class(obj, a_class):
     "xcvzxvzxc"
     if isinstance(obj, a_class) and not isinstance(obj, bool):
         return True
+    elif obj == None:
+        return False
     else:
         return False
