c2570326e6e985ff1a23724e8a95da1d0df73579
Your Name <you@example.com>
2025-12-28 14:46:17 +0000

    gitpusher

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,8 +5,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     "sdfsdfs"
+
+
     def __init__(self, size):
-        self.integer_validator(size)
+        self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
