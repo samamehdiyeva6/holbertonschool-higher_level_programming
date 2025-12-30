31a6b8ca0695e15b8c3a4ce61e57c0bbd22c6efe
Sama Mehdiyeva <105722149+samamehdiyeva6@users.noreply.github.com>
2025-12-31 00:33:16 +0400

    Update task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -15,9 +15,7 @@ class VerboseList(list):
         super().remove(item)
 
     def pop(self, index = -1):
-        item = self[index]
-        if item is not None:
-            print(f"Popped {item} from the list.")
-            super().pop(index)
-        else:
-            raise AssertionError
+    item = super().pop(index)
+    print(f"Popped {item} from the list.")
+    return item   
+
