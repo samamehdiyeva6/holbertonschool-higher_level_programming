1140515af10f13409cd6508dba64bdf97ee4ce3f
Sama Mehdiyeva <105722149+samamehdiyeva6@users.noreply.github.com>
2026-01-01 21:11:12 +0400

    Update task_03_countediterator.py

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 class CountedIterator:
-    "hgjgfkj"
-    def __init__(self, iterable):
-        self.iterable = iter(iterable)
-        self.count = 0
+    """Iterator that counts the number of iterations."""
 
-    def get_count(self):
-        return self.count
+    def __init__(self, iterable):
+        self._iterable = iter(iterable)
+        self._count = 0
 
     def __next__(self):
         item = next(self._iterable)
         self._count += 1
         return item
+
+    def get_count(self):
+        return self._count
