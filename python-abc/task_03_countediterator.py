7c91b81a02457c0a5cac715e55bb2c24f8fc9da5
Sama Mehdiyeva <105722149+samamehdiyeva6@users.noreply.github.com>
2026-01-01 21:10:29 +0400

    Update task_03_countediterator.py

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,10 +9,7 @@ class CountedIterator:
     def get_count(self):
         return self.count
 
-    def __iter__(self):
-        return self
-
     def __next__(self):
-    value = next(self.iterable)   # element götür
-    self.count += 1               # SAYI ARTIR
-    return value
+        item = next(self._iterable)
+        self._count += 1
+        return item
