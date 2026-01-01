3223a945881f1d810ea215af6be7be4abf9886b9
Sama Mehdiyeva <105722149+samamehdiyeva6@users.noreply.github.com>
2026-01-01 20:55:56 +0400

    Update task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class CountedIterator:
+    "hgjgfkj"
+    def __init__(self, iterable):
+        self.iterable = iter(iterable)
+        self.count = 0
+
+    def get_count(self):
+        return self.count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.iterable)
