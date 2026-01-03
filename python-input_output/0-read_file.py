32704fffb5d5d736c30ee3d502b8f1413b6f7e0f
Your Name <you@example.com>
2026-01-03 21:31:43 +0000

    gitpusher

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 "hgjgjg"
-
 def read_file(filename=""):
     "hfjfjf"
-    with open (filename, encoding = "utf-8") as f:
+    with open (filename, encoding="utf-8") as f:
         print(f.read(), end="")
