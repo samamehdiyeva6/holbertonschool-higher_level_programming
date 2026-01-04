b04b9a7601d014b0a7672a148267b582351081da
Your Name <you@example.com>
2026-01-04 19:09:13 +0000

    gitpusher

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,5 +3,5 @@
 
 def append_write(filename="", text=""):
     "sdfsdf"
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
