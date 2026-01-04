08298965dd410b16e3c2ecfac8b25ebcabd1ebb7
Your Name <you@example.com>
2026-01-04 19:02:51 +0000

    gitpusher

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,5 +4,10 @@
 
 def write_file(filename="", text=""):
     "fsdbhfsjk"
-    with open(filename, 'w', encoding="utf-8") as f:
-        print(f.write(text))
+    try:
+        with open(filename, 'w', encoding="utf-8") as f:
+            return f.write(text)
+    except FileNotFoundError:
+        with open(filename, 'w', encoding="utf-8") as f:
+            return f.write(text)
+    
