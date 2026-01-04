36f724a13b1b2ccda0dc9650c57fbd310869a61e
Your Name <you@example.com>
2026-01-04 21:21:07 +0000

    gitpusher

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     "hjvjf"
     with open(filename, 'w', encoding="utf-8") as f:
-        return json.dump(filename, my_obj)
+        return json.dump(my_obj, f)
