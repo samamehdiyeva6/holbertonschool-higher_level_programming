b5f868c4b7e66f8b5793fbde1a1583221c9ab0f1
Your Name <you@example.com>
2026-01-05 14:00:34 +0000

    gitpusher

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"dsgdsg"
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w', encoding="utf-8") as f:
+        return json.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
