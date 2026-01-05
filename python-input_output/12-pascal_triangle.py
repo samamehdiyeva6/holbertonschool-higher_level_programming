7797f5b62a444cecc9d3bf34111f529c8efc2887
Your Name <you@example.com>
2026-01-05 13:04:22 +0000

    gitpusher

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"sdfsdfds"
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        triangle = []
+        for i in range(n):
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            if i > 0:
+                row.append(1)
+
+            triangle.append(row)
+
+    return triangle
+        
