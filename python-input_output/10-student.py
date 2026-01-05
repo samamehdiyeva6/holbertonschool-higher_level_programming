805952a85283586d95deeed0641ea501e92c8cb6
Your Name <you@example.com>
2026-01-05 10:51:56 +0000

    gitpusher

---
+++ python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"sdfsdfds"
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if isinstance(attr, str):
+                    self.__dict__.keys()
+        else:
+            return self.__dict__
