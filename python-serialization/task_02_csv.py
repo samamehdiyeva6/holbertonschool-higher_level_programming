bad2aaf4efbd3ac4c5809ecd89ed66ff666d842d
Your Name <you@example.com>
2026-01-06 14:28:52 +0000

    gitpusher

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,11 @@
+python import csv import json
+
+def convert_csv_to_json(filename):
+    with open(filename, newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+    try:
+        with open(data.json, "w", encoding="utf-8") as f:
+            json.dump(reader, f)
+            return True
+    except FileNotFoundError:
+        return False
