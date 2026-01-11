c81034decd1769fa545345bdcf9e263835d225fd
Your Name <you@example.com>
2026-01-11 17:15:43 +0000

    gitpusher

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+    else:
+        print("Failed to fetch posts")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        dict = []
+            for post in posts:
+                dict.append({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
+
+        with open("posts.csv", mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictReader(file, fieldnames = ["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(dict)
+
+        print("Posts successfully saved to posts.csv")
+    else:
+        print("Failed to fetch posts")
