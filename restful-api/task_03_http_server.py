3acd4fbf88ed8ea3a5596f0cd22ff1d0116dd79f
Your Name <you@example.com>
2026-01-11 23:15:37 +0400

    gitpusher

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,15 +3,15 @@ import json
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/data":
-            data = {"name": "John", "age": 30, "city": "New York"}
+        if self.path == "/":
+            data = {"message": "Hello, this is a simple API!"}
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode())
 
-        elif self.path == "/":
-            data = {"message": "Hello, this is a simple API!"}
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
