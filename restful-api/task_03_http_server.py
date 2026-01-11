ef1c1df35c517299c47098c851191052d2d0d412
Your Name <you@example.com>
2026-01-11 23:07:52 +0400

    gitpusher

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -2,11 +2,24 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello")
+        if self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(b"Hello")
 
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not Found")
 
 server = HTTPServer(("localhost", 8000), SimpleAPIHandler)
+print("Server running on http://localhost:8000")
 server.serve_forever()
