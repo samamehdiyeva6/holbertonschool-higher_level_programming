9f1e75d8036bc3504d4d2bf748b38bdd23aeb9a4
Your Name <you@example.com>
2026-01-13 15:30:23 +0400

    gitpusher

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
-)
+);
 
-INSERT INTO IF EXISTS second_table(id, name, score)
+INSERT INTO second_table(id, name, score)
 VALUES(1, 'John', 10),
-VALUES(1, 'Alex', 3),
-VALUES(1, 'Bob', 14),
-VALUES(1, 'George', 8)
+(1, 'Alex', 3),
+(1, 'Bob', 14),
+(1, 'George', 8)
 
