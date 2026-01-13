7ea123d8c0a8577a092d18063018578d906633d1
Your Name <you@example.com>
2026-01-13 15:26:10 +0400

    gitpusher

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,12 @@
--- SDJLKFHDS
+-- hfjhsgdjm
 CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
 )
+
+INSERT INTO second_table(id, name, score)
+VALUES(1, 'John', 10),
+VALUES(1, 'Alex', 3),
+VALUES(1, 'Bob', 14),
+VALUES(1, 'George', 8)
