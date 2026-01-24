ace6da8e7c16eabc0ab8672a6e56073bb9690f56
Your Name <you@example.com>
2026-01-24 18:02:19 +0400

    gitpusher

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- fedsfsd
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT NOT NULL AUTO INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT,
     status_id INT NOT NULL,
     name VARCHAR(256),
     PRIMARY KEY(id),
