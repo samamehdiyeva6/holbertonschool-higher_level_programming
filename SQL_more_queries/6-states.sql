89ac7cff48484d4a95c2cfe7e8621f82a3e3a3c2
Your Name <you@example.com>
2026-01-24 17:32:32 +0400

    gitpusher

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- ggfhdg
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
