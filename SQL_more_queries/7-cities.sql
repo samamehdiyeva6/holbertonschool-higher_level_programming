fcb4c47dfea5357b51d1a80abf8260271479a555
Your Name <you@example.com>
2026-01-24 18:08:26 +0400

    gitpusher

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,7 @@
 -- fedsfsd
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
