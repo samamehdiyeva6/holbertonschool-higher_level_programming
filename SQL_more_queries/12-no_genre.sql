1b75aeb3b6225107b5ea3e960e7426b46d1c7a35
Your Name <you@example.com>
2026-01-26 14:35:14 +0400

    update

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- dsfsdf
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id WHERE genre_id=NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id WHERE tv_show_genres.genre_id=NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
