070aad09945587bd514d55259f0e09a00e2cdacd
Your Name <you@example.com>
2026-01-26 13:22:55 +0400

    update

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- djfksh
-SELECT cities.id, cities.name, states.name FROM cities WHERE cities.state_id=SELECT id FROM states ORDER BY cities.id
+SELECT cities.id, cities.name, states.name FROM cities ORDER BY cities.id
