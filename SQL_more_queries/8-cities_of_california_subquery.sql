7ca94846bd6d608a24ae4bc7bf2cad44737cd2f5
Your Name <you@example.com>
2026-01-26 13:10:56 +0400

    update

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- djfksh
-SELECT id, name FROM hbtn_0d_usa.cities cities.id=(SELECT id FROM states WHERE states.name='California' LIMIT 1) ORDER BY cities.id
+SELECT id, name FROM hbtn_0d_usa.cities WHERE cities.id=(SELECT id FROM states WHERE states.name='California' LIMIT 1) ORDER BY cities.id
