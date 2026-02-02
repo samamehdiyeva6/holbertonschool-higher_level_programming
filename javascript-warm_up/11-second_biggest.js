c5525b0337ff06b05305f0b578eeee9413bc6dcb
Your Name <you@example.com>
2026-02-03 02:38:07 +0400

    gitpusher

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 const list = process.argv.slice(2);
-let max = Number(list[0]);
-for (let i = 1; i < list.length; i++) {
-  if (max < Number(list[i])) {
-    max = Number(list[i]);
-  }
-}
-console.log(max);
+const sorted = list.sort();
+console.log(Number(sorted[-2]));
