9554c622163de4ef2b934db8e5fa0038ff2255b5
Your Name <you@example.com>
2026-02-03 01:52:23 +0400

    gitpusher

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let x=0; x < list.length; x++) {
+for (let x = 0; x < list.length; x++) {
   console.log(list[x]);
 }
