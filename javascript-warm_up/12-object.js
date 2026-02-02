4e1d5687cf9944db09eec97dacaec0b4df741457
Your Name <you@example.com>
2026-02-03 02:58:05 +0400

    gitpusher

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let myObject1 = {
+const myObject1 = {
   type: 'object',
   value: 12
 };
-let myObject2 = {
+const myObject2 = {
   type: 'object',
   value: 12
-  };
+};
 myObject2.value = 89;
 console.log(myObject1);
 console.log(myObject2);
