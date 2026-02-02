b07dedfeb90aa768b7d7148c19739a28e01d35a1
Your Name <you@example.com>
2026-02-03 01:28:07 +0400

    gitpusher

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 const args = process.args.slice(2);
-if (args.length === 0) {
-  console.log('No argument');
-} else {
-  console.log(args);
-}
+console.log(args[0], 'No argument');
