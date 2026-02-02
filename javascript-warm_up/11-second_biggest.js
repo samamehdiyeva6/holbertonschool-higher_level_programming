4453db81e55565a432f9add38a400885383120f9
Your Name <you@example.com>
2026-02-03 02:35:30 +0400

    gitpusher

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const list = Number(process.argv.slice(2));
-let max = list[0];
+const list = process.argv.slice(2);
+let max = Number(list[0]);
 for (let i = 1; i < list.length; i++) {
-  if (max < list[i]) {
-    max = list[i];
+  if (max < Number(list[i])) {
+    max = Number(list[i]);
   }
 }
 console.log(max);
