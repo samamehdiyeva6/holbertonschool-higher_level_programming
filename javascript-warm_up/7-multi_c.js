2a63bba3e2be35d95efd0d166eb1ffecfde9f3e6
Your Name <you@example.com>
2026-02-03 02:02:10 +0400

    gitpusher

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const x = process.argv.slice(2);
 const num = Number(x) || 'Missing number of occurrences';
-for (let i = 0; i < num; num++) {
-  console.log('C is fun' * num);
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
 }
