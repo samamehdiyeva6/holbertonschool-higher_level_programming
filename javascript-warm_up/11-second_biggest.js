398bfd61df7e14a42e757d840bcf988e6a78b519
Your Name <you@example.com>
2026-02-03 02:43:21 +0400

    gitpusher

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = process.argv.slice(2).map(Number);
-const sorted = list.sort();
-const reversed = sorted.reverse();
+const sorted = list((a,b) => b - a);
 console.log(reversed[1]);
