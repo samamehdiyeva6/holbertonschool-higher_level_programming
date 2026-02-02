8dda31f7a8bb42a8fcede468189ee3c048ca52f8
Your Name <you@example.com>
2026-02-03 02:18:10 +0400

    gitpusher

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b){
-    return a + b;
+function add (a, b) {
+  return a + b;
 }
 const a = Number(process.argv.slice(2)[0]);
 const b = Number(process.argv.slice(2)[1]);
