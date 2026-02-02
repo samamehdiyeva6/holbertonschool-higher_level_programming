a8ecd5a8b809015c83337900906fd3fb0baffcf3
Your Name <you@example.com>
2026-02-03 01:46:41 +0400

    gitpusher

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (Number.isInteger(args[0])) {
+if (Number.isInteger(Number(args[0]))) {
   console.log('My number: ' + args[0]);
 } else {
     console.log('Not a number');
