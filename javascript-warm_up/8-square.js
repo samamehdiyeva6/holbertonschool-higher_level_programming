068345830556c1543de47ec44f8b9f390b933b41
Your Name <you@example.com>
2026-02-03 02:11:09 +0400

    gitpusher

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 const size = process.argv.slice(2);
-const num = Number(size) || 'Missing size';
+const num = Number(size);
 let res = '';
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size;j++) {
-      res += 'X';
+if (Number.isInteger(num)) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size;j++) {
+          res += 'X';
+        }
+        console.log(res);
+        res = '';
     }
-    console.log(res);
-    res = '';
+} else {
+    console.log('Missing size');
 }
