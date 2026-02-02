ff02adfebf933a6fef74ab7ce46e925cf933efa2
Your Name <you@example.com>
2026-02-03 02:49:36 +0400

    gitpusher

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = process.argv.slice(2).map(Number);
-if (list.length === 1) {
+if (list.length === 1 || list.length === 0) {
   console.log(0);
 } else {
   list.sort((a, b) => b - a);
