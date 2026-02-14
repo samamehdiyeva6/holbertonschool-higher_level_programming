28f9f4f2775ebd8831d18e37821c055d566e1543
Your Name <you@example.com>
2026-02-14 21:59:06 +0400

    gitpusher

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+import MySQLdb
+conn = MySQLdb.connect(host="localhost", port=3306, user="sama'@'localhost", passwd="mypassword", db="hbtn_0e_0_usa")
+cur = conn.cursor()
+cur.execute("SELECT id, name FROM states ORDER BY states.id")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
