aed4bc47c5658ef494239a28c7ecc4f0de4fe74a
Your Name <you@example.com>
2026-02-15 14:23:28 +0400

    Fix: use sys.argv and if __name__ == '__main__':

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"scdhbvjsdcs"
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
-conn = MySQLdb.connect(host="localhost", port=3306, user="sama'@'localhost", passwd="mypassword", db="hbtn_0e_0_usa")
-cur = conn.cursor()
-cur.execute("SELECT id, name FROM states ORDER BY states.id")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
-# Testing git push alias
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
