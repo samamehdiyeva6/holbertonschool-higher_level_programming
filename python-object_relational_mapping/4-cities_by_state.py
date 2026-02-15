4deba1078491060af9d6aed552aa50a475d86c97
Your Name <you@example.com>
2026-02-16 00:27:31 +0400

    gitpusher

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM cities INNER JOIN states ON cities.state_id==states.id
+    cur.execute("""SELECT * FROM cities INNER JOIN states ON cities.state_id=states.id
                 ORDER BY id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
