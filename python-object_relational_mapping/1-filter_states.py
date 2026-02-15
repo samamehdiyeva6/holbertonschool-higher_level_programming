59021066822088b72b9efaab550718305a0c81c5
Your Name <you@example.com>
2026-02-15 23:24:31 +0400

    gitpusher

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     "dsfdsfs"
     conn = MySQLdb.connect(host="localhost", port=3306, 
-                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf-8")
+                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
