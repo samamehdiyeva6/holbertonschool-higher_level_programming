d5ea18423a38d700f21c65f4c042baeb7a418a6c
Your Name <you@example.com>
2026-02-16 00:11:03 +0400

    gitpusher

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute(str.format("""SELECT * FROM states
-                WHERE name='{}' ORDER BY id ASC""", sys.argv[4]))
+                WHERE name LIKE BINARY'{}' ORDER BY id ASC""", sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
