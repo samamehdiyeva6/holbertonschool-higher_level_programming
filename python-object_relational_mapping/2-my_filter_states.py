a3496a8f4c64418a2cf7205c807479011e645677
Your Name <you@example.com>
2026-02-16 00:03:59 +0400

    gitpusher

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states
-                WHERE name=:name ORDER BY id ASC""", {"name": sys.argv[4]})
+    cur.execute(str.format("""SELECT * FROM states
+                WHERE name={name} ORDER BY id ASC""", sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
