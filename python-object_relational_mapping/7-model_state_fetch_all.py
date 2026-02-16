8423d9c7858cb2e46e4b30abff414c7fe818a5aa
Your Name <you@example.com>
2026-02-16 18:44:45 +0400

    gitpusher

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"dsfsdfsd"
+"""Lists all State objects from a MySQL database"""
 
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
     username = sys.argv[1]
-    password =  sys.argv[2]
+    password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
-        pool_pre_ping = True
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()  
 
     for state in states:
         print(f"{state.id}: {state.name}")
