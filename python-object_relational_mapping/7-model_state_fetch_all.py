668886c5fa94e52df5cf5239431774052692196f
Your Name <you@example.com>
2026-02-16 18:45:44 +0400

    gitpusher

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()  
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
