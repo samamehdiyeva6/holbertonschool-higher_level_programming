bba9cdf5d7471a2d8ed9c2b1460550d1ad9f7483
Your Name <you@example.com>
2026-02-16 15:45:23 +0400

    gitpusher

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"vsdfsdfds"
+
+from sqlalchemy import create_engine, Column, Integer, String, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+engine = create_engine("sqlite:///states.db")
+
+Base.metadata.create_all(engine)
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+Session = sessionmaker(bind = engine)
+session = Session()
