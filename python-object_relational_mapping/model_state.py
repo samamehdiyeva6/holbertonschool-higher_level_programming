718e551e6c643e82061250b2058ff54f45a8a309
Your Name <you@example.com>
2026-02-16 15:47:32 +0400

    gitpusher

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 "vsdfsdfds"
 
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, sessionmaker
 
 Base = declarative_base()
 engine = create_engine("sqlite:///states.db")
