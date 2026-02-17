cb861c4aac4d1c72156696d14bb9a980c9aca9af
Your Name <you@example.com>
2026-02-17 21:51:56 +0400

    gitpusher

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Lists all State objects from a MySQL database"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}: {password}@localhost:3306/{database}",
+        pool_pre_ping=True
+        )
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = sys.argv[4]
+    session.commit()
+
+    session.close()
