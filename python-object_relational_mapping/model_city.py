0ee065b593bd33ed4081b26ee9fa18456187ed79
Your Name <you@example.com>
2026-02-17 23:21:39 +0400

    gitpusher

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"sdfsdfs"
+
+
+import MySQLdb
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
+engine = create_engine("sqlite://cities.db")
+
+Base.metadata.create_all(engine)
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+Session = sessionmaker(engine)
+session = Session()
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 engine = create_engine("sqlite:///states.db")
@@ -16,6 +16,7 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
 
 
 Session = sessionmaker(bind=engine)
