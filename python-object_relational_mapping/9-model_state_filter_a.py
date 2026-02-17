13019175492498159061e7736a50f6a453680fd6
Your Name <you@example.com>
2026-02-17 20:29:26 +0400

    gitpusher

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.contains("a")).order_by(State.id).all()
+    states = session.query(State).filter(State.name.contains("a"))
     for state in states:
         print(f"{state.id}: {state.name}")
 
