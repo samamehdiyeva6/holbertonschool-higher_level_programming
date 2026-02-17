237c55a81fd00262e64b2e7df8862419cdb36002
Your Name <you@example.com>
2026-02-17 20:16:25 +0400

    gitpusher

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
-    pool_pre_ping=True
+        pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
