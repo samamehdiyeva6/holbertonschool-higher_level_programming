4167668571e078c69c71dd9586342bcb49ba3414
Your Name <you@example.com>
2026-02-17 20:00:34 +0400

    gitpusher

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists all State objects from a MySQL database"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import State, Base
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+    f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+    pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    session.close()
